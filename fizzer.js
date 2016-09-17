de8fdf034aae6a116086104ee04fa2731090d968
scoobycoder <scoobycoder@gmail.com>
2016-09-17 17:03:50 -0400

    1 3 5 6 10 15 0 all working

--- fizzer.js
+++ fizzer.js
@@ -1,11 +1,17 @@
 var fizz = function(number = 0) {
-	if (number === 3) {
+	if (number === 0) {
+		return 0;
+	}
+	if ((number % 15) === 0) {
+		return "fizzbuzz";
+	}
+	if ((number % 3) === 0) {
 		return "fizz";
 	}
-	if (number === 5) {
+	if ((number % 5) === 0) {
 		return "buzz";
 	}
-  return number;
+	return number;
 };
 
 exports.fizzer = fizz;
