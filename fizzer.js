6dbd923bcce08b394362e8f6618e3210c2d6d9a4
scoobycoder <scoobycoder@gmail.com>
2016-09-17 16:53:27 -0400

    fizz and buzz basic passing

--- fizzer.js
+++ fizzer.js
@@ -1,4 +1,10 @@
-var fizz = function(number) {
+var fizz = function(number = 0) {
+	if (number === 3) {
+		return "fizz";
+	}
+	if (number === 5) {
+		return "buzz";
+	}
   return number;
 };
 
--- spec/fizz_spec.js
+++ spec/fizz_spec.js
@@ -9,9 +9,25 @@ describe('fizz buzz', function(){
 	assert.equal(fizz.fizzer(1), 1, 'number 1 should equal number 1');
     done();
   })
-  
+
   it('2 should return number 2', function(done){
 	assert.equal(fizz.fizzer(2), 2, 'number 2 should equal number 2');
     done();
   })  
+
+  it('should return zero when nothing passed to it', function(done){
+	assert.equal(fizz.fizzer(), 0, 'return zero when nothing passed to it');
+    done();
+  }) 
+
+  it('should return fizz for 3', function(done){
+	assert.equal(fizz.fizzer(3), 'fizz', 'return fizz for 3');
+    done();
+  }) 
+
+  it('should return buzz for 5', function(done){
+	assert.equal(fizz.fizzer(5), 'buzz', 'return buzz for 5');
+    done();
+  })
+
 })
