ae38a39595368b71cec77ea1991a1ec9efe3251b
na5j2t4 <nathan_halterman@gap.com>
2022-03-13 17:02:24 -0400

    starting point for kata

--- fizzer.js
+++ fizzer.js
@@ -1,16 +1,5 @@
-const fizz = (number) => {
-	if (isFizzBuzz(number)) return "fizzbuzz"
-	if (isFizz(number, 3)) return "fizz"
-	if (isBuzz(number, 5)) return "buzz"
-	
-	return number;
-};
+const fizz = () => {
 
-const isFizzBuzz = (number) => buzzes(number) && fizzes(number)
-const isFizz = (number, singleNumber) => fizzes(number) || includesNumber(number, singleNumber)
-const isBuzz = (number, singleNumber) => buzzes(number) || includesNumber(number, singleNumber)
-const includesNumber = (number, includedNumber) => number.toString().includes(includedNumber)
-const buzzes = (number) => (number % 5) === 0
-const fizzes = (number) => (number % 3) === 0
+};
 
 exports.fizzer = fizz;
--- spec/fizz_spec.js
+++ spec/fizz_spec.js
@@ -2,50 +2,9 @@ var chai = require("chai");
 var fizz = require ("../fizzer")
 var assert = chai.assert;
 
-
 describe('fizz buzz', function(){
-  it('1 should return number 1', function(done){
-	  assert.equal(fizz.fizzer(1), 1, 'number 1 should equal number 1');
-    done();
-  })
-
-  it('2 should return number 2', function(done){
-	  assert.equal(fizz.fizzer(2), 2, 'number 2 should equal number 2');
-    done();
-  })   
-
-  it('should return fizz for 3', function(done){
-	  assert.equal(fizz.fizzer(3), 'fizz', 'return fizz for 3');
-    done();
-  }) 
-
-  it('should return buzz for 5', function(done){
-	  assert.equal(fizz.fizzer(5), 'buzz', 'return buzz for 5');
-    done();
-  })
-
-  it('should return fizz for 6', function(done){
-	  assert.equal(fizz.fizzer(6), 'fizz', 'return fizz for 6');
-    done();
-  })  
-
-  it('should return buzz for 10', function(done){
-	  assert.equal(fizz.fizzer(10), 'buzz', 'return buzz for 10');
-    done();
-  })
-
-  it('should return fizzbuzz for 15', function(done){
-	  assert.equal(fizz.fizzer(15), 'fizzbuzz', 'return fizzbuzz for 15');
-    done();
-  })
-
-  it('should return fizz for 13', function(done){
-	  assert.equal(fizz.fizzer(13), 'fizz', 'return fizz for 13');
-    done();
-  })
-
-  it('should return buzz for 58', function(done){
-	  assert.equal(fizz.fizzer(58), 'buzz', 'return buzz for 58');
+  it('do somehting', function(done){
+	  assert.equal(true, false);
     done();
   })
 
